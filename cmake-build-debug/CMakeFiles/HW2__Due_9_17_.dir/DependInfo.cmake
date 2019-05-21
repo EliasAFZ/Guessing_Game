92ffb21ae475efaf40624941476d09dad75dc437
Elias <slicer360@live.com>
2019-05-21 18:13:17 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/HW2__Due_9_17_.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/College/Older semesters/Fall 2018/itec-310 (C and Unix)/HW/HW2 (Due 9-17)/as2.c" "D:/College/Older semesters/Fall 2018/itec-310 (C and Unix)/HW/HW2 (Due 9-17)/cmake-build-debug/CMakeFiles/HW2__Due_9_17_.dir/as2.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/HW2__Due_9_17_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW2__Due_9_17_.dir/as2.c.obj"
+  "HW2__Due_9_17_.pdb"
+  "HW2__Due_9_17_.exe"
+  "HW2__Due_9_17_.exe.manifest"
+  "libHW2__Due_9_17_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HW2__Due_9_17_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
